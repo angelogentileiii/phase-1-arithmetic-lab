e9ac3b0afe64c2bf191deda35de9614a3309eeff
Angelo Gentile III <angelo.john.gentile@gmail.com>
2023-10-02 13:01:59 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+let multiply, num1, num2;
 
-// Write your code here
+num1 = 31;
+num2 = 2;
+
+multiply = num1 * num2;
+
+let random;
+
+random = Math.floor(Math.random() * 10);
+
+let mod, num3, num4;
+
+num3 = 9;
+num4 = 5;
+
+mod = num3 - num4;
+
+let max;
+
+max = Math.max(0, 5, 10, 15, 20);
